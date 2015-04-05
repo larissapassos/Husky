842153e693410a5bef993776e14d9c74b985682a
larissapassos <lnpa@cin.ufpe.br>
2015-04-05 17:02:53 -0400

    Zip and map

--- foldTest.cpp
+++ foldTest.cpp
@@ -1,11 +1,63 @@
+#include <chrono>
 #include <cstdio>
-#include <iostream>
 #include <fstream>
-#include <vector>
 #include <functional>
+#include <iostream>
+#include <random>
+#include <tuple>
+#include <vector>
 #include "functionalibrary.h"
 
 using namespace functionalibrary;
+
+static int N = 10;
+
+// Class that encapsulates a pseudo-random generator
+class Rand_Int {
+public:
+	Rand_Int(unsigned seed, int low, int high) :en{ seed }, dist{ low, high } {}
+	int operator()() { return dist(en); }
+private:
+	std::minstd_rand en;
+	std::uniform_int_distribution<int> dist;
+};
+
+// Method to print a container
+template <typename Cont>
+void printCont(const Cont& c) {
+	for (auto x : c) {
+		std::cout << x << " ";
+	}
+	std::cout << "\n";
+}
+
+// Method to print a tuple container
+template <typename Cont>
+void printTupleCont(const Cont& c) {
+	for (auto tpl : c) {
+		std::cout << "(" << std::get<0>(tpl) << ", " << std::get<1>(tpl) << ") ";
+	}
+	std::cout << "\n";
+}
+
+// Method to fill a container
+template <typename Cont>
+void fillCont(Cont& c, Rand_Int& rd) {
+	int size = (rd() % N);
+	for (int i = 0; i < N; ++i) {
+		c.push_back(rd() % 100);
+	}
+}
+
+// Functor for testing
+struct addx{
+	addx(int x) :x(x) {}
+	int operator()(int y) { return x + y; }
+
+private:
+	int x;
+};
+
 
 int main() {
 
@@ -19,6 +71,27 @@ int main() {
 	file << sum << std::endl;
 
 	file.close();
+
+	// random seed
+	unsigned seed = static_cast<unsigned>(std::chrono::system_clock::now().time_since_epoch().count());
+	Rand_Int rd{ seed, 1, std::numeric_limits<int>::max() };
+
+	// Populating vector of ints
+	std::vector<int> v1;
+	fillCont(v1, rd);
+
+	// Testing map
+	std::cout << "v1: ";
+	printCont(v1);
+	addx add42{ 42 };
+	std::cout << "Mapping v1 to add42: ";
+	std::vector<int> v2 = map(v1, add42);
+	printCont(v2);
+
+	// Testing zip
+	auto v3 = zip(v1, v2);
+	std::cout << "zipping v1 and v2: ";
+	printTupleCont(v3);
 
 	return 0;
 }
--- functionalibrary.h
+++ functionalibrary.h
@@ -1,6 +1,7 @@
 #ifndef _FUNCTIONALIBRARY_H
 #define _FUNCTIONALIBRARY_H
 
+#include <algorithm>
 #include <numeric>
 
 namespace functionalibrary {
@@ -8,6 +9,26 @@ namespace functionalibrary {
 	template<typename Function, typename Iterator, typename T>
 	static T foldl(Iterator first, Iterator last, T init, Function f) { return std::accumulate(first, last, init, f); }
 
+	template <typename Cont,typename UnaryOperation>
+	Cont map(const Cont& c, UnaryOperation f) { 
+		Cont c2;
+		for (auto elm : c) {
+			auto new_elm = f(elm);
+			c2.push_back(new_elm);
+		}
+		return c2;
+	}
+
+	template <typename Cont1, typename Cont2>
+	std::vector<std::pair <typename Cont1::value_type, typename Cont2::value_type> > zip(const Cont1& c1, const Cont2& c2) {
+		std::vector<std::pair <typename Cont1::value_type, typename Cont2::value_type> > result;
+		int len = std::min(c1.size(), c2.size());
+		for (int i = 0; i < len; ++i) {
+			auto new_elm = std::make_pair(c1[i], c2[i]);
+			result.push_back(new_elm);
+		}
+		return result;
+	}
 }
 
 #endif /* _FUNCTIONALIBRARY_H */
