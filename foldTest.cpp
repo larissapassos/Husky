c79966ec8a258a9c72a558890db7148127033da1
vinimoraesrc <vmrc@cin.ufpe.br>
2015-04-05 17:43:14 -0400

    Special folds and sample tutorial.

--- foldTest.cpp
+++ foldTest.cpp
@@ -2,21 +2,46 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cassert>
 #include <functional>
 #include "functionalibrary.h"
-
-using namespace functionalibrary;
 
 int main() {
 
 	std::vector<int> vec{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	std::vector<int> vec2{ 1, 2, 3, 4, 5 };
+	std::vector<bool> vec_true{ true, true, true, true, true };
+	std::vector<bool> vec_false{ false, false, false, false, false };
+	std::vector<bool> vec_mixed{ true, false, true, false, true };
 
 	std::ofstream file;
 	file.open("tests.txt");
 
-	int sum = foldl(vec.begin(), vec.end(), 0, std::plus<int>());
+	int sum = functionalibrary::sum(vec.begin(), vec.end(), 0);
+
+	bool test_and_true = functionalibrary::and(vec_true.begin(), vec_true.end());
+	bool test_and_false = functionalibrary::and(vec_false.begin(), vec_false.end());
+	bool test_and_false2 = functionalibrary::and(vec_mixed.begin(), vec_mixed.end());
+
+	bool test_or_true = functionalibrary::or(vec_true.begin(), vec_true.end());
+	bool test_or_false = functionalibrary::or(vec_false.begin(), vec_false.end());
+	bool test_or_true2 = functionalibrary::or(vec_mixed.begin(), vec_mixed.end());
 
 	file << sum << std::endl;
+
+	assert(test_and_true);
+	assert(!test_and_false);
+	assert(!test_and_false2);
+
+	assert(test_or_true);
+	assert(test_or_true2);
+	assert(!test_or_false);
+
+	file << functionalibrary::product(vec2.begin(), vec2.end(), 1);
+	file << '\n';
+
+	file << functionalibrary::product(vec2.begin(), vec2.end(), -1);
+	file << '\n';
 
 	file.close();
 
--- functionalibrary.h
+++ functionalibrary.h
@@ -1,12 +1,27 @@
 #ifndef _FUNCTIONALIBRARY_H
 #define _FUNCTIONALIBRARY_H
 
+#include <functional>
 #include <numeric>
 
 namespace functionalibrary {
 
 	template<typename Function, typename Iterator, typename T>
-	static T foldl(Iterator first, Iterator last, T init, Function f) { return std::accumulate(first, last, init, f); }
+	T foldl(Iterator first, Iterator last, T init, Function f) { return std::accumulate(first, last, init, f); }
+
+	template<typename Iterator, typename T>
+	T sum(Iterator first, Iterator last, T init) { return std::accumulate(first, last, init); }
+
+	template<typename Iterator, typename T>
+	T product(Iterator first, Iterator last, T init) { return std::accumulate(first, last, init, std::multiplies<T>()); }
+
+	template<typename Iterator>
+	bool and(Iterator first, Iterator last) { return std::accumulate(first, last, true, std::logical_and<bool>()); }
+
+	template<typename Iterator>
+	bool or(Iterator first, Iterator last) { return std::accumulate(first, last, false, std::logical_or<bool>()); }
+
+	// Look into concat and concatMap
 
 }
 
